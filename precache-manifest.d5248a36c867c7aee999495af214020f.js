44427979efa22bd3da0168daaed7d80c03ef7b3f
tommydev <tommtyjr700@aol.xom>
2019-12-16 12:30:46 -0500

    Updates

---
+++ precache-manifest.d5248a36c867c7aee999495af214020f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86954eb2dac99f91e524dd97bddd1fef",
+    "url": "/TommyDevPublic/soccer-highlights/index.html"
+  },
+  {
+    "revision": "e02237cdfd9ffa8ce1d3",
+    "url": "/TommyDevPublic/soccer-highlights/static/css/2.5246d9b3.chunk.css"
+  },
+  {
+    "revision": "62af35d9f04c36f35949",
+    "url": "/TommyDevPublic/soccer-highlights/static/css/main.407e085e.chunk.css"
+  },
+  {
+    "revision": "e02237cdfd9ffa8ce1d3",
+    "url": "/TommyDevPublic/soccer-highlights/static/js/2.945a7fef.chunk.js"
+  },
+  {
+    "revision": "62af35d9f04c36f35949",
+    "url": "/TommyDevPublic/soccer-highlights/static/js/main.6c7ef941.chunk.js"
+  },
+  {
+    "revision": "7dab6dc548b1d314ea4d",
+    "url": "/TommyDevPublic/soccer-highlights/static/js/runtime-main.1a7f3455.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "ed1b94f135a396cf87992852af7a3946",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/neilsenlogo-580x358.ed1b94f1.png"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/TommyDevPublic/soccer-highlights/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
